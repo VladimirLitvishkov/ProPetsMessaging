9806ddd21340ef255c642f965b204cc19382766b
Vladimir <Vladimir@Vladimir5>
2020-05-06 14:35:32 +0300

    update 04.05.2020

--- ProPetsMessaging/src/main/java/propets/controller/messaging/MessagingController.java
+++ ProPetsMessaging/src/main/java/propets/controller/messaging/MessagingController.java
@@ -2,6 +2,7 @@ package propets.controller.messaging;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import propets.dto.messaging.PostDto;
 import propets.dto.messaging.PostRequestDto;
 import propets.service.messaging.MessagingService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/{lang}/v1")
 public class MessagingController {
